17ec67636126cc5da2e140e39ebac4d89b25ebaf
guclerc <48179473+guclerc@users.noreply.github.com>
2025-05-18 15:18:09 +0200

    Create meautocraft_config.lua

---
+++ meautocraft_config.lua
@@ -0,0 +1,15 @@
+-- meautocraft_config.lua
+-- Fichier de configuration pour les items à surveiller et leurs seuils
+
+return {
+    {
+        name = "minecraft:iron_ingot",
+        label = "Iron Ingot",
+        threshold = 256
+    },
+    {
+        name = "minecraft:glass",
+        label = "Glass",
+        threshold = 128
+    }
+}
